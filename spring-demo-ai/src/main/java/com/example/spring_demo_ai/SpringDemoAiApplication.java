1af4ad86e877014f466eeffed4439bcb1ae81565
congth2001 <vuquoccong01@gmail.com>
2024-06-06 15:51:41 +0700

    Update Face Recognition Service

--- spring-demo-ai/src/main/java/com/example/spring_demo_ai/SpringDemoAiApplication.java
+++ spring-demo-ai/src/main/java/com/example/spring_demo_ai/SpringDemoAiApplication.java
@@ -5,13 +5,16 @@ import java.util.List;
 
 import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
+// import org.nd4j.linalg.util.Paths;
 import org.opencv.core.*;
 import org.opencv.core.Core.MinMaxLocResult;
 import org.opencv.dnn.*;
 import org.opencv.utils.*;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.objdetect.Objdetect;
 import org.opencv.videoio.VideoCapture;
+import org.slf4j.LoggerFactory;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
@@ -23,63 +26,60 @@ import org.opencv.core.Size;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// import ai.djl.ModelException;
+// import ai.djl.inference.Predictor;
+// import ai.djl.modality.cv.Image;
+// import ai.djl.modality.cv.ImageFactory;
+// import ai.djl.modality.cv.output.DetectedObjects;
+// import ai.djl.repository.zoo.Criteria;
+// import ai.djl.repository.zoo.ZooModel;
+// import ai.djl.training.util.ProgressBar;
+// import ai.djl.translate.TranslateException;
+
+// import org.slf4j.Logger;
+// import java.io.IOException;
+// import java.nio.file.Files;
+// import java.nio.file.Path;
 @SpringBootApplication
 public class SpringDemoAiApplication {
 	public static void main(String[] args) {
+		System.loadLibrary( Core.NATIVE_LIBRARY_NAME ); 
+		int absoluteFaceSize = 0;
 		// // Load the OpenCV library
-		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		String modelWeights = "spring-demo-ai/weights/facedetection/yolov8n.onnx"; 
-		Net net = Dnn.readNetFromONNX(modelWeights); 
-        String imgPath = "spring-demo-ai/images/yolo.png";
-		Mat img = Imgcodecs.imread(imgPath);
-		if (img.empty()) {
+		MatOfRect faces = new MatOfRect();
+		Mat grayFrame = new Mat();
+
+		Mat frame = Imgcodecs.imread("C:/Users/Admin/Downloads/archive/val/elton_john/httpcdncdnjustjaredcomwpcontentuploadsheadlineseltonjohnstillstandingbrooklynnewyearsjpg.jpg");
+		if (frame.empty()) {
             System.out.println("Can not read img.");
             return;
         }
-
-		Mat blob = Dnn.blobFromImage(img, 1/255.0, new Size(640, 640));
-		net.setInput(blob);
-		Mat predict = net.forward();
-		Mat mask = predict.reshape(0, 1).reshape(0, predict.size(1));
-
-		double width = img.cols()/640.0;
-		double height = img.rows()/640.0;
-		Rect2d[] rect2d = new Rect2d[mask.cols()];
-		float[] scoref = new float[mask.cols()];
-		int[] classid = new int[mask.cols()];
 		
-		for(int i = 0; i < mask.cols(); i++) {
-			Mat score = mask.col(i).submat(4, predict.size(1) - 1, 0, 1);
-			MinMaxLocResult mmr = Core.minMaxLoc(score);
-			scoref[i] = (float) mmr.maxVal;
-			classid[i] = (int) mmr.maxLoc.y;
-			double[] x = mask.col(i).get(0, 0);
-			double[] y = mask.col(i).get(1, 0);
-			double[] w = mask.col(i).get(2, 0);
-			double[] h = mask.col(i).get(3, 0);
-			rect2d[i] = new Rect2d((x[0]-w[0]/2)*width, (y[0]-h[0]/2)*height, w[0]*width, h[0]*height);
-		}
-		try {
-			MatOfRect2d bboxes = new MatOfRect2d(rect2d);
-			MatOfFloat scores = new MatOfFloat(scoref);
-			MatOfInt indices = new MatOfInt();
-			
-			Dnn.NMSBoxes(bboxes, scores, 0.5f,0.5f, indices);
-			List<Integer> result = indices.toList();
-			
-			for (Integer integer : result) {
-				if(classid[integer] == 0) { 
-					Imgproc.rectangle(img, new Rect(rect2d[integer].tl(), rect2d[integer].size()),
-								new Scalar(255, 0, 0), 1);
-					Imgproc.putText(img, "person:" + scoref[integer], rect2d[integer].tl(), 
-							Imgproc.FONT_HERSHEY_SIMPLEX, 0.5, new Scalar(0, 255, 0));
-				}
+		//convert the frame in gray scale
+		Imgproc.cvtColor(frame, grayFrame, Imgproc.COLOR_BGR2GRAY);
+		Imgproc.equalizeHist(grayFrame, grayFrame);
+		
+		if(absoluteFaceSize==0) {
+			int height = grayFrame.height();
+			if(Math.round(height * 0.2f) > 0) {
+				absoluteFaceSize = Math.round(height * 0.2f);
 			}
-		} catch (Exception e) {
-			System.out.println("No person in image");
 		}
+		
+		//detect faces
+		CascadeClassifier faceCascade = new CascadeClassifier();
+		faceCascade.load("spring-demo-ai/data/haarcascade_frontalface_alt2.xml");
+		faceCascade.detectMultiScale(grayFrame, faces, 1.1, 2, 0|Objdetect.CASCADE_SCALE_IMAGE, 
+									new Size(absoluteFaceSize,absoluteFaceSize), new Size()	);
+		// System.err.println(faces);
+		Rect[] faceArray = faces.toArray();
+		System.out.println(faceArray.length);
+		for(int i = 0; i < faceArray.length; i++) {
+			Imgproc.rectangle(frame, faceArray[i], new Scalar(0,0,255), 3);
+		}
+		// Imgcodecs.imwrite("out.jpg",grayFrame);
 
-        HighGui.imshow("img", img);
+        HighGui.imshow("img", frame);
 		HighGui.resizeWindow("img", 600, 600);
         HighGui.waitKey();
 
